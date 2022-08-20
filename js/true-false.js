2a1289308fe065773a2ddf474695f8a7ebb6f92e
yousufislame <online.okk@gmail.com>
2022-08-20 22:46:48 +0600

    true & false condutaion of JavaScript

---
+++ js/true-false.js
@@ -0,0 +1,13 @@
+// let name = "Yousuf";
+// let name;
+// let number = 12;
+// let number = 0;
+
+let check = 10;
+
+if (check > 5) {
+   console.log("condition is true");
+} else {
+   console.log("condition is false");
+
+}
