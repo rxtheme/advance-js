0418832f7e011fa993698c90ba5eb6b632bfb848
yousufislame <online.okk@gmail.com>
2022-08-21 09:26:02 +0600

    null vs undefined

---
+++ js/null-undefined.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+   return num1 + num2;
+}
+
+const result = add(5, 5);
+
+console.log(result);
+
+
+
+const fun = undefined;
+
+console.log(fun);
+
+
+const ages = [23, 12, 26]; //5 is not available
+
+console.log(ages[5]);
+
